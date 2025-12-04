d7dac298635b3b1ff46439f634a7a900cb7e9821
Brian Tucker <111880851+briantuckerdesign@users.noreply.github.com>
2025-12-04 11:36:10 -0500

    Update main.js

--- dist/main.js
+++ dist/main.js
@@ -258,12 +258,6 @@ ${notificationTypes}
 ${animations}
 ${icons}
 
-body {
-  font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
-    Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif,
-    sans-serif;
-}
-
 .nu_notification-container {
   position: fixed; 
   top: 0.5rem;
